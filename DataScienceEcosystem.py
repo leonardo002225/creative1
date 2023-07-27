2db3b5b4fdfc57be3ef5c9be277c172749a33ce8
leonardo002225 <140709187+leonardo002225@users.noreply.github.com>
2023-07-27 14:08:45 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ## Objectives
+# 
+# 1. Create a Jupyter Notebook
+# 2. Insert code and markdown cells
+# 3. Share your notebook through GitHub
+
+# ## Some of the popular languages that Data Scientists use are:
+# 1. Python.
+# 2. R.
+# 3. SQL.
+# 4. Java.
+# 5. Julia.
+# 6. Scala.
+# 7. C/C++.
+# 8. JavaScript.
+
+# ## Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. TensorFlow.
+# 2. NumPy.
+# 3. SciPy.
+# 4. Pandas.
+# 5. Matplotlib.
+# 6. Keras.
+# 7. SciKit-Learn.
+# 8. PyTorch.
+# 9. Scrapy.
+# 10. BeautifulSoup.
+# 11. LightGBM.
+# 12. ELI5.
+# 13. Theano.
+# 14. NuPIC.
+# 15. Ramp.
+# 16. Pipenv.
+# 17. Bob.
+# 18. PyBrain.
+# 19. Caffe2.
+# 20. Chainer.
+
+# ## Data Science Tools:
+# 
+# | Data Science Tools |
+# |------------------|
+# | SAS. It is one of those data science tools which are specifically designed for statistical operation |
+# | Apache Spark |
+# | BigM |
+
+# In[2]:
+
+
+5+4
+
+
+# In[3]:
+
+
+10+57
+
+
+# In[4]:
+
+
+(20*5)/4
+
+
+# In[5]:
+
+
+54.25/27
+
+
+# In[6]:
+
+
+(3*4)+5
+
+
+# In[8]:
+
+
+days = 0
+hours = 0
+mins = 0
+
+time = 200
+#days = time / 1440
+leftover_minutes = time % 1440
+hours = leftover_minutes / 60
+#mins = time - (days*1440) - (hours*60)
+print(str(days) + " days, " + str(hours) + " hours, " + str(mins) +  " mins. ")
+
+
+# ## Author
+# Leonardo Zambrano
+# 
+
+# In[ ]:
+
+
+
+
